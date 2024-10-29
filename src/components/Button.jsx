736d6a6fbe2e19ad027295a5e85f4964770c0458
heyheynic <nicsayhey@gmail.com>
2024-10-29 09:42:01 +0100

    first tailwind exercise, pop-up cookie banner

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = () => {
+    return ( 
+
+  
+        <p className="text-lg bg-slate-50 hover:bg-blue-200 hover:outline-2 hover:outline-slate-50 hover:scale-105 transition-all text-black text-center px-auto py-2 mx-2 rounded-xl cursor-pointer active:bg-teal-500 active:text-cyan-50">I like Cookies</p>
+  
+    );
+}
+ 
+export default Button;

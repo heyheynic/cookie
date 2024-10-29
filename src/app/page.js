9de3c3e5db9fdd9b89948346a33221c0693a8d31
heyheynic <nicsayhey@gmail.com>
2024-10-29 13:30:25 +0100

    did some things in class

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Popup from "@/components/Popup";
 export default function Home() {
   return (
     <div className="relative h-screen w-screen">
-      <Popup styling="absolute bottom-4 right-4"/>
+      <Popup styling="absolute bottom-4 right-4" />
     </div>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,7 +7,6 @@ const Popup = ({ styling }) => {
     <section
       className={`${styling} text-white bg-black rounded-3xl p-5 max-w-72 grid grid-rows-[auto,1fr,2fr,auto]`}
     >
-     
       <IoCloseOutline className="w-6 h-6 justify-self-end transition-all cursor-pointer hover:text-blue-200  hover:scale-110 active:text-teal-500" />
       <FaCookieBite className="w-10 h-10 scale-x-[-1]" />
       {/* <div className="mx-2 grid grid-cols-2 pb-4">
@@ -17,7 +16,7 @@ const Popup = ({ styling }) => {
       <h2 className=" text-2xl mx-2 place-self-end py-5 ">
         We use cookies to improve your experience.
       </h2>
-      <Button />
+      <Button buttonText="I like Cookies" />
     </section>
   );
 };
